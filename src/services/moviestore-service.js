7d90dbf2f98f5a4410802e4a8b256a0130f9f834
AlexLeeUA <alexlitkevych.dev@gmail.com>
2019-04-04 22:07:38 +0300

    Uploaded the project

---
+++ src/services/moviestore-service.js
@@ -0,0 +1,48 @@
+export default class MoviestoreService {
+
+    getResource = async (url) => {
+        const res = await fetch(url);
+        return await res.json();
+    }    
+    
+    getImagePath = async (id) => {
+        const poster = await this.getResource(`https://api.themoviedb.org/3/movie/${id}?api_key=dab311d55ca033353e291532b0572824&language=en-US`);
+        const base = await this.getResource('https://api.themoviedb.org/3/configuration?api_key=dab311d55ca033353e291532b0572824');
+        const size = await this.getResource('https://api.themoviedb.org/3/configuration?api_key=dab311d55ca033353e291532b0572824');
+    
+        const part1 = base.images.secure_base_url;
+        const part2 = size.images.poster_sizes[4];
+        const part3 = poster.poster_path;
+            
+        const imagePath = `${part1}${part2}${part3}`;
+        return imagePath;
+    }   
+    
+    getItemList = async () => {
+        const list = await this.getResource('https://api.themoviedb.org/4/list/222?page=1&api_key=dab311d55ca033353e291532b0572824');
+         return list.results.map(this._tranformItem);
+    }
+
+    getItem = async (id) => {
+        const item = await this.getResource(`https://api.themoviedb.org/3/movie/${id}?api_key=dab311d55ca033353e291532b0572824&language=en-US`);
+        return this._tranformItem(item);
+    }
+
+    _tranformItem = (movie) => {
+        return  {
+            id: movie.id,
+            overview: movie.overview,
+            title: movie.original_title,
+            release: movie.release_date
+        }  
+    } 
+}
+
+const x = new MovapiService();
+x.getImagePath(8827).then((value) => {
+    console.log(value)
+});
+
+x.getItem(8827).then((value) => {
+    console.log(value)
+});
